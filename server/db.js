e6268d9119d9bf307ae179d20ff870e038638147
Lesley Reuter <lesley.reuter@gmail.com>
2021-05-11 17:00:10 -0400

    add ssl to db.js

--- server/db.js
+++ server/db.js
@@ -2,6 +2,12 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: !process.env.DATABASE_URL.includes('localhost') ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  } : {}
 });
 
 sequelize.authenticate().then(
